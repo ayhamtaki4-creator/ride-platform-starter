b9fdec9c869d0a918e657082f059a5fd97055c39
Ayham Altaki <ayhamtaki4@gmail.com>
2026-08-07 19:40:22 +0300

    feat: register route editing service

--- apps/api/src/tracking/tracking.module.ts
+++ apps/api/src/tracking/tracking.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TrackingController } from './tracking.controller';
 import { TrackingService } from './tracking.service';
+import { TripRouteEditingService } from './trip-route-editing.service';
 
 @Module({
   controllers: [TrackingController],
-  providers: [TrackingService],
+  providers: [TrackingService, TripRouteEditingService],
   exports: [TrackingService]
 })
 export class TrackingModule {}
