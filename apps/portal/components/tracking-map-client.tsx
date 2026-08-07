03e4cb537c22041dfd2ce08925bfef5221debe80
Ayham Altaki <ayhamtaki4@gmail.com>
2026-08-07 18:25:31 +0300

    add dynamic tracking map wrapper

---
+++ apps/portal/components/tracking-map-client.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import type { TrackingMapProps } from "./tracking-map";
+
+const DynamicTrackingMap = dynamic(() => import("./tracking-map"), {
+  ssr: false,
+  loading: () => <div className="map-loading" role="status">جارٍ تحميل خريطة الرحلة...</div>,
+});
+
+export function TrackingMapClient(props: TrackingMapProps) {
+  return <DynamicTrackingMap {...props} />;
+}
