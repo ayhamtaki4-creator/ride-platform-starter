89db0a162aa57c83719d36f0fe8de63c7e0816c8
Ayham Altaki <ayhamtaki4@gmail.com>
2026-08-09 01:14:05 +0300

    feat: reuse redis limiter for maps

--- apps/api/src/maps/maps.module.ts
+++ apps/api/src/maps/maps.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { MapsController } from './maps.controller';
 import { MapsService } from './maps.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [MapsController],
   providers: [MapsService],
   exports: [MapsService]
