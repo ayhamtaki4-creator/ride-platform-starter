bc34c878a50f61a26e48e41962938f0b422c1539
Ayham Altaki <ayhamtaki4@gmail.com>
2026-08-08 14:58:07 +0300

    feat: enable public ticket analysis

--- apps/api/src/bookings/bookings.module.ts
+++ apps/api/src/bookings/bookings.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { BookingsController } from './bookings.controller';
 import { BookingsService } from './bookings.service';
 import { FlightTicketExtractorService } from './flight-ticket-extractor.service';
 import { FlightTicketsService } from './flight-tickets.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [BookingsController],
   providers: [BookingsService, FlightTicketExtractorService, FlightTicketsService]
 })
