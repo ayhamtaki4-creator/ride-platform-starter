d3107b26d6fdade57a69e318d45fcaff6c68245f
Ayham Altaki <ayhamtaki4@gmail.com>
2026-08-08 20:08:02 +0300

    feat: export GPS ingress throttle

--- apps/api/src/tracking/tracking.module.ts
+++ apps/api/src/tracking/tracking.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { LocationIngressThrottleService } from './location-ingress-throttle.service';
 import { TrackingController } from './tracking.controller';
 import { TrackingService } from './tracking.service';
 import { TripRouteEditingService } from './trip-route-editing.service';
 
 @Module({
   controllers: [TrackingController],
-  providers: [TrackingService, TripRouteEditingService],
-  exports: [TrackingService]
+  providers: [TrackingService, TripRouteEditingService, LocationIngressThrottleService],
+  exports: [TrackingService, LocationIngressThrottleService]
 })
 export class TrackingModule {}
