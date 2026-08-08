9bf2f519888104f501d385fc698ead67bb05a7e6
Ayham Altaki <ayhamtaki4@gmail.com>
2026-08-08 20:08:26 +0300

    feat: share GPS throttle with realtime gateway

--- apps/api/src/realtime/realtime.module.ts
+++ apps/api/src/realtime/realtime.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { TrackingModule } from '../tracking/tracking.module';
 import { RealtimeEventsService } from './realtime-events.service';
 import { RealtimeGateway } from './realtime.gateway';
 
 @Global()
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TrackingModule],
   providers: [RealtimeEventsService, RealtimeGateway],
   exports: [RealtimeEventsService]
 })
