391a7d7dcfbb2fdb99237b2f8cd434fdfeab6ef0
Ayham Altaki <ayhamtaki4@gmail.com>
2026-08-09 01:49:31 +0300

    feat: register resilient maps cache

--- apps/api/src/maps/maps.module.ts
+++ apps/api/src/maps/maps.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { MapsCacheService } from './maps-cache.service';
 import { MapsController } from './maps.controller';
 import { MapsService } from './maps.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [MapsController],
-  providers: [MapsService],
+  providers: [MapsCacheService, MapsService],
   exports: [MapsService]
 })
 export class MapsModule {}
