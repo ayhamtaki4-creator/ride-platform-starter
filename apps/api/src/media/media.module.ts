dd9fe7cf36f8319cfe854db26f813b2018ed5187
Ayham Altaki <ayhamtaki4@gmail.com>
2026-08-08 03:00:04 +0300

    feat: register R2 media storage

--- apps/api/src/media/media.module.ts
+++ apps/api/src/media/media.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { MediaController } from './media.controller';
 import { MediaService } from './media.service';
+import { R2ObjectStorageService } from './r2-object-storage.service';
 
 @Global()
 @Module({
   controllers: [MediaController],
-  providers: [MediaService],
-  exports: [MediaService]
+  providers: [MediaService, R2ObjectStorageService],
+  exports: [MediaService, R2ObjectStorageService]
 })
 export class MediaModule {}
