525cfd72daa991e4ae025728b4cfa12b6e3c67ce
Ayham Altaki <ayhamtaki4@gmail.com>
2026-08-09 05:10:00 +0300

    perf: isolate booking-only styles and client code

---
+++ apps/portal/components/booking-form-lazy-content.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import "react-datepicker/dist/react-datepicker.css";
+import "@/app/booking-mobile.css";
+import { BookingForm } from "./booking-form";
+
+export default function BookingFormLazyContent() {
+  return <BookingForm />;
+}
