02efd63fef326f67841662ecfda83c5df78b5241
Ayham Altaki <ayhamtaki4@gmail.com>
2026-08-11 01:08:04 +0300

    feat: wire mobile push into realtime events

--- apps/api/src/realtime/realtime.module.ts
+++ apps/api/src/realtime/realtime.module.ts
@@ -1,12 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { MobilePushModule } from '../mobile-push/mobile-push.module';
 import { TrackingModule } from '../tracking/tracking.module';
 import { RealtimeEventsService } from './realtime-events.service';
 import { RealtimeGateway } from './realtime.gateway';
 
 @Global()
 @Module({
-  imports: [AuthModule, TrackingModule],
+  imports: [AuthModule, TrackingModule, MobilePushModule],
   providers: [RealtimeEventsService, RealtimeGateway],
   exports: [RealtimeEventsService]
 })
